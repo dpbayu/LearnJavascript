8fac60838af720cc072f41a9f0c770165c5ee8d1
dpbayu <dwiputrabayu19@gmail.com>
2022-03-23 13:13:08 +0700

    Update script.js

--- latihan 2/js/script.js
+++ latihan 2/js/script.js
@@ -2,7 +2,12 @@ var jmlMobil = 20;
 var noMobil = 1;
 var mobilBeroperasi = 12;
 
-while (noMobil <= jmlMobil) {
-    console.log('Mobil no. '+ noMobil +' beroperasi dengan baik');
-noMobil++;
+while (noMobil <= mobilBeroperasi) {
+    console.log('Mobil no. ' + noMobil + ' beroperasi dengan baik');
+    noMobil++;
+};
+
+for (noMobil = mobilBeroperasi + 1; noMobil <= jmlMobil; noMobil++){
+    console.log('Mobil no. '+ noMobil + ' sedang tidak beroperasi dengan baik');
+    noMobil++;
 };
