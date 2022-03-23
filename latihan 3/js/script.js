bd69fe8ad0b556b45d047075a062d8ac0801abe6
dpbayu <dwiputrabayu19@gmail.com>
2022-03-23 13:37:16 +0700

    Create script.js

---
+++ latihan 3/js/script.js
@@ -0,0 +1,11 @@
+var jmlMobil = 20;
+var mobilBeroperasi = 12;
+var noMobil = 1;
+
+for (noMobil; noMobil <= jmlMobil; noMobil++) {
+    if (noMobil <= 12) {
+        console.log('Mobil no. ' + noMobil + ' beroperasi dengan baik');
+    } else {
+        console.log('Mobil no. ' + noMobil + ' sedang tidak beroperasi');
+    }
+}
