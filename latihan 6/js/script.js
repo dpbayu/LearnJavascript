fe5def7a5e8aba482239aa017fbf3fa1570f4586
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2022-03-29 16:11:08 +0700

    Update script.js

--- latihan 6/js/script.js
+++ latihan 6/js/script.js
@@ -1,41 +1,41 @@
 // Bintang mengurut dari kecil ke besar //
-// var s = '';
-// var angka = '5';
-// for (var i = 1; i <= angka; i++) {
-//     for (var j = 0; j < i; j++) {
-//         s += "*";
-//     }
-//     s += '\n';
-// }
-// console.log(s);
+var s = '';
+var angka = '5';
+for (var i = 1; i <= angka; i++) {
+    for (var j = 0; j < i; j++) {
+        s += "*";
+    }
+    s += '\n';
+}
+console.log(s);
 // Bintang mengurut dari kecil ke besar //
 // Bintang mengurut dari besar ke kecil //
-// var s = '';
-// var angka = '5';
-// for (var i = angka; i > 0; i--) {
-//     for (var j = 0; j < i; j++) {
-//         s += "*";
-//     }
-//     s += '\n';
-// }
-// console.log(s);
+var s = '';
+var angka = '5';
+for (var i = angka; i > 0; i--) {
+    for (var j = 0; j < i; j++) {
+        s += "*";
+    }
+    s += '\n';
+}
+console.log(s);
 // Bintang mengurut dari besar ke kecil //
 // Bintang dari kecil ke besar & dari besar ke kecil //
-// var s = '';
-// var angka = '5';
-// for (var i = 1; i <= angka; i++) {
-//     for (var j = 0; j < i; j++) {
-//         s += "*";
-//     }
-//     s += '\n';
-// }
-// for (var i = angka; i > 0; i--) {
-//     for (var j = 0; j < i; j++) {
-//         s += "*";
-//     }
-//     s += '\n';
-// }
-// console.log(s);
+var s = '';
+var angka = '5';
+for (var i = 1; i <= angka; i++) {
+    for (var j = 0; j < i; j++) {
+        s += "*";
+    }
+    s += '\n';
+}
+for (var i = angka; i > 0; i--) {
+    for (var j = 0; j < i; j++) {
+        s += "*";
+    }
+    s += '\n';
+}
+console.log(s);
 // Bintang dari kecil ke besar & dari besar ke kecil //
 // Bintang muncul dari kanan //
 var s = '';
@@ -54,14 +54,37 @@ console.log(s);
 // Bintang muncul dari kanan mengurut dari terbesar hingga terkecil //
 var s = '';
 var angka = '5';
-for (var i = angka; i < 0; i--) {
-    for (var j = angka - 1; j >= i; j++) {
-        s += ' ';
+for (var i = 1; i <= angka; i++) {
+    for (j = 1; j <= i - 1; j++) {
+        s += " ";
     }
-    for (var k = 1; k < i; k++) {
+    for (k = 1; k <= angka - i + 1; k++) {
         s += "*";
     }
-    s + '\n';
+    s += '\n';
 }
 console.log(s);
 // Bintang muncul dari kanan mengurut dari terbesar hingga terkecil //
+// Bintang muncul dari kanan dan dari kecil ke besar & dari besar ke kecil //
+var s = '';
+var angka = '5';
+for (var i = 1; i <= angka; i++) {
+    for (var j = angka - 1; j >= i; j--) {
+        s += ' ';
+    }
+    for (var k = 1; k <= i; k++) {
+        s += "*";
+    }
+    s += '\n';
+}
+for (var i = 1; i <= angka; i++) {
+    for (j = 1; j <= i - 1; j++) {
+        s += " ";
+    }
+    for (k = 1; k <= angka - i + 1; k++) {
+        s += "*";
+    }
+    s += '\n';
+}
+console.log(s);
+// Bintang muncul dari kanan dan dari kecil ke besar & dari besar ke kecil //
