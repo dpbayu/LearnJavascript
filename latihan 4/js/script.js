20fd67e4903844c21b0b65a22210d93f83571815
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2022-03-29 12:56:47 +0700

    Update script.js

--- latihan 4/js/script.js
+++ latihan 4/js/script.js
@@ -3,19 +3,19 @@ var mobilBeroperasi = 12;
 var noMobil = 1;
 
 // Version 1 //
-// for (noMobil; noMobil <= jmlMobil; noMobil++) {
-//     if (noMobil <= 12 && noMobil !== 7) {
-//         console.log('Mobil no. ' + noMobil + ' beroperasi dengan baik')
-//     } else if (noMobil === 18) {
-//         console.log('Mobil no. ' + noMobil + ' sedang lembur')
-//     } else if (noMobil === 20) {
-//         console.log('Mobil no. ' + noMobil + ' sedang lembur')
-//     } else if (noMobil === 7) {
-//         console.log('Mobil no. ' + noMobil + ' sedang lembur')
-//     } else {
-//         console.log('Mobil no. ' + noMobil + ' sedang tidak beroperasi')
-//     }
-// }
+for (noMobil; noMobil <= jmlMobil; noMobil++) {
+    if (noMobil <= 12 && noMobil !== 7) {
+        console.log('Mobil no. ' + noMobil + ' beroperasi dengan baik')
+    } else if (noMobil === 18) {
+        console.log('Mobil no. ' + noMobil + ' sedang lembur')
+    } else if (noMobil === 20) {
+        console.log('Mobil no. ' + noMobil + ' sedang lembur')
+    } else if (noMobil === 7) {
+        console.log('Mobil no. ' + noMobil + ' sedang lembur')
+    } else {
+        console.log('Mobil no. ' + noMobil + ' sedang tidak beroperasi')
+    }
+}
 // Version 1 //
 // Version 2 //
 for (noMobil; noMobil <= jmlMobil; noMobil++) {
