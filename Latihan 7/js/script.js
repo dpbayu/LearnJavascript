17f5dcf178631b97f636404bea0013c4dea132b9
Dwi Putra Bayu <74140729+dpbayu@users.noreply.github.com>
2022-04-27 10:30:14 +0700

    initial commit

--- Latihan 7/js/script.js
+++ Latihan 7/js/script.js
@@ -1,26 +1,32 @@
-// Tentukan pilihan player
-var p = prompt('Pilih : Gajah, Manusia, Semut');
-// Tentukan pilihan computer
-var comp = Math.random();
-if (comp > 0.34) {
-    comp = 'Gajah';
-} else if (comp >= 0.34 && comp < 0.67) {
-    comp = 'Manusia';
-} else {
-    comp = 'Semut';
+var tanya = true;
+while (tanya) {
+    // Tentukan pilihan player
+    var p = prompt('Pilih : GAJAH, MANUSIA, SEMUT');
+    // Tentukan pilihan computer
+    var comp = Math.random();
+    if (comp > 0.34) {
+        comp = 'GAJAH';
+    } else if (comp >= 0.34 && comp < 0.67) {
+        comp = 'MANUSIA';
+    } else {
+        comp = 'SEMUT';
+    }
+    // Buat rules permainan
+    var hasil = '';
+    if (p == comp) {
+        hasil = 'SERI';
+    } else if (p == 'GAJAH') {
+        hasil = (comp == 'MANUSIA') ? 'MENANG!' : 'KALAH!';
+    } else if (p == 'MANUSIA') {
+        hasil = (comp = 'GAJAH') ? 'KALAH!' : 'MENANG!';
+    } else if (p == 'SEMUT') {
+        hasil(comp = 'MANUSIA') ? 'KALAH!' : 'MENANG!';
+    } else {
+        hasil = 'Anda memasukkan pilihan yang salah!!';
+    }
+    // Tentukan hasil
+    alert('Kamu memilih : ' + p + ' dan komputer memilih : ' + comp + '\n Maka hasilnya : Kamu ' + hasil);
+    // Konfirmasi
+    tanya = confirm('Mau main lagi?')
 }
-// Buat rules permainan
-var hasil = '';
-if (p == comp) {
-    hasil = 'SERI';
-} else if (p == 'Gajah') {
-    hasil = (comp == 'Manusia') ? 'MENANG!' : 'KALAH!';
-} else if (p == 'Manusia') {
-    hasil = (comp = 'Gajah') ? 'KALAH!' : 'MENANG!';
-} else if (p == 'Semut') {
-    hasil(comp = 'Manusia') ? 'KALAH!' : 'MENANG!';
-} else {
-    hasil = 'Anda memasukkan pilihan yang salah!!';
-}
-// Tentukan hasil
-alert('Kamu memilih : ' + p + ' dan komputer memilih : ' + comp + '\n Maka hasilnya : Kamu ' + hasil);
+alert('Terima Kasih telah bermain.')
