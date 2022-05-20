81c5786d116f86b78c8d8db37ade8cb707dfe2fa
Dwi Putra Bayu <74140729+dpbayu@users.noreply.github.com>
2022-05-20 10:00:33 +0700

    initial commit

--- DOM/Part 1 (Selection)/script.js
+++ DOM/Part 1 (Selection)/script.js
@@ -7,9 +7,16 @@ judul.innerHTML = 'Dwi Putra Bayu';
 // document.getElementsByTagName() -> HTML Collection (Array)
 const p = document.getElementsByTagName('p');
 for (let i = 0; i < p.length; i++) {
-    p[i].style.backgroundColor = 'gray';
+    p[i].style.backgroundColor = '#';
 }
 // p[0].style.backgroundColor = 'lightblue';
 // p[1].style.backgroundColor = 'lightblue';
 // p[2].style.backgroundColor = 'lightblue';
 // p[3].style.backgroundColor = 'lightblue';
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '50px';
+
+// document.getElementsByClassName() -> HTML Collection (Array)
+const p1 = document.getElementsByClassName('p1');
+p1[0].innerHTML = 'Ini diubah dari javascript';
