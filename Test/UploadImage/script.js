dc03037b861c78fef19b5b76028ebf58a2f9caa0
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2022-05-28 23:31:26 +0700

    initial commit

--- Test/UploadImage/script.js
+++ Test/UploadImage/script.js
@@ -11,3 +11,27 @@
 //     reader.readAsDataURL(this.files[0]);
 // })
 // Version 1 //
+
+// Version 2 //
+$(".image-box").click(function (event) {
+    var previewImg = $(this).children("img");
+    $(this)
+        .siblings()
+        .children("input")
+        .trigger("click");
+    $(this)
+        .siblings()
+        .children("input")
+        .change(function () {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var urll = e.target.result;
+                $(previewImg).attr("src", urll);
+                previewImg.parent().css("background", "transparent");
+                previewImg.show();
+                previewImg.siblings("p").hide();
+            };
+            reader.readAsDataURL(this.files[0]);
+        });
+});
+// Version 2 //
