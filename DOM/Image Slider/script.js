c00813f72072cdaea439de72ef44eb9ce4439a1d
Dwi Putra Bayu <74140729+dpbayu@users.noreply.github.com>
2022-06-13 15:17:59 +0700

    initial commit

---
+++ DOM/Image Slider/script.js
@@ -0,0 +1,7 @@
+const conainer = document.querySelector('.container');
+
+CredentialsContainer.addEventListener('click',function(e){
+    if(e.target.className=='thumb'){
+        // Cek apakah yang di klik adalah thumb 
+    }
+})
